71994be9b152301ee9947bfbb0293481f4fd28ea
Meredith Lind <meredith.lind@atomicobject.com>
2023-11-03 11:20:46 -0400

    Add jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+}
+
+export default config
